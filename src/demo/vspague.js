8afb7a0dbba7e4291b13c08abb87f8ba66603326
Abner Rodrigues <abnerluis1001@gmail.com>
2022-04-14 12:45:38 -0300

    refactor vspague.js to use classes

--- src/demo/vspague.js
+++ src/demo/vspague.js
@@ -1,288 +1,198 @@
-var ln_font_size = 4;
-var in_sequencial = 2;
-var in_sequencial_executar = 0;
-var io_connection = Conectar();
-var io_tags;
-
-io_connection.onopen = function () {
-  Trace("Connection successful");
-  io_tags = new Tags();
-  io_tags.TagsInicializar();
-};
-
-io_connection.onerror = function (error) {
-  Trace("Disconnected");
-  Trace(error.data);
-};
-
-io_connection.onmessage = function (e) {
-  Trace(e.data);
-  io_tags.TagsInicializar();
-
-  // Mosta as tag's recebidas.
-  new ServicoDesmontar(e.data);
-
-  // Se retorno não for de pacote ok...
-  if (io_tags.retorno !== "0") {
-    in_sequencial = io_tags.sequencial;
-
-    document.getElementById("io_txt_sequencial").value = in_sequencial;
+class VSPagueClient {
+  constructor() {
+    this.in_sequencial = 2;
+    this.in_sequencial_executar = 0;
+    this.io_connection = null;
+    this.io_tags = null;
   }
 
-  // Guarda o sequencial corrente da coleta.
-  in_sequencial_executar = io_tags.automacao_coleta_sequencial;
+  send(message) {
+    this.io_connection.send(message);
+    return message;
+  }
 
-  // Apresenta o comprovante..
-  if (io_tags.transacao_comprovante_1via !== "") {
-    alert(
-      io_tags.transacao_comprovante_1via + io_tags.transacao_comprovante_2via
+  next() {
+    this.in_sequencial = this.in_sequencial + 1;
+    return this.in_sequencial;
+  }
+
+  init() {
+    return this.send(
+      'servico="iniciar"sequencial="' +
+        this.next() +
+        '"retorno="1"versao="1.0.0"aplicacao_tela="VBIAutomationTest"aplicacao="V$PagueClient"'
     );
   }
-};
 
-// Função para envio de pacotes.
-function Send(as_buffer) {
-  // Envia o pacote.
-  io_connection.send(as_buffer);
-
-  var obj = document.getElementById("output");
-  obj.innerHTML =
-    "<br> >>> SEND: <br>" +
-    as_buffer.fontsize(ln_font_size) +
-    "<br><br>" +
-    obj.innerHTML;
-}
-
-// Função para visualização dos pacotes trocados.
-function Trace(as_buffer) {
-  var obj = document.getElementById("output");
-  obj.innerHTML =
-    "<br> <<< RECEIVE: <br>" +
-    as_buffer.fontsize(ln_font_size) +
-    "<br><br>" +
-    obj.innerHTML;
-}
-
-function Coleta(ao_event) {
-  if (ao_event == "" || ao_event.keyCode === 13) {
-    Send(
-      'automacao_coleta_sequencial="' +
-        in_sequencial_executar +
-        '"automacao_coleta_retorno="0"automacao_coleta_informacao="' +
-        document.getElementById("io_txt_coleta_informacao").value +
-        '"'
+  end() {
+    return this.send(
+      'servico="finalizar"sequencial="' + this.next() + '"retorno="1"'
     );
-
-    document.getElementById("io_txt_coleta_informacao").value = "";
-  }
-}
-
-function Iniciar() {
-  Send(
-    'servico="iniciar"sequencial="' +
-      Sequencial() +
-      '"retorno="1"versao="1.0.0"aplicacao_tela="VBIAutomationTest"aplicacao="V$PagueClient"'
-  );
-}
-
-function Finalizar() {
-  Send('servico="finalizar"sequencial="' + Sequencial() + '"retorno="1"');
-}
-
-/**
-		Função responsável por montar e enviar o serviço "executar".
-		*/
-function Executar() {
-  var ls_tags_executar =
-    'servico="executar"retorno="1"sequencial="' + Sequencial() + '"';
-
-  var ls_tipo_cartao =
-    document.getElementById("io_lst_tipo_cartao").options[
-      io_lst_tipo_cartao.selectedIndex
-    ].text;
-
-  var ls_tipo_transacao = document.getElementById("io_lst_transacao_tipo")
-    .options[io_lst_transacao_tipo.selectedIndex].text;
-
-  var ls_tipo_produto = document.getElementById("io_lst_tipo_produto").options[
-    io_lst_tipo_produto.selectedIndex
-  ].text;
-
-  var ls_transacao_pagamento = document.getElementById(
-    "io_lst_transacao_pagamento"
-  ).options[io_lst_transacao_pagamento.selectedIndex].text;
-
-  var ls_transacao_valor = document.getElementById("io_txt_coleta_valor").value;
-
-  if (ls_transacao_valor !== "") {
-    ls_transacao_valor = 'transacao_valor="' + ls_transacao_valor + '"';
-    ls_tags_executar = ls_tags_executar + ls_transacao_valor;
   }
 
-  if (io_lst_transacao_tipo.selectedIndex >= 0) {
-    ls_tipo_transacao = 'transacao="' + ls_tipo_transacao + '"';
-    ls_tags_executar = ls_tags_executar + ls_tipo_transacao;
-  }
-
-  if (io_lst_tipo_cartao.selectedIndex >= 0) {
-    ls_tipo_cartao = 'transacao_tipo_cartao="' + ls_tipo_cartao + '"';
-    ls_tags_executar = ls_tags_executar + ls_tipo_cartao;
-  }
-
-  if (io_lst_transacao_pagamento.selectedIndex >= 0) {
-    ls_transacao_pagamento =
-      'transacao_pagamento="' + ls_transacao_pagamento + '"';
-    ls_tags_executar = ls_tags_executar + ls_transacao_pagamento;
-  }
-
-  if (io_lst_tipo_produto.selectedIndex >= 0) {
-    ls_tipo_produto = 'transacao_produto="' + ls_tipo_produto + '"';
-    ls_tags_executar = ls_tags_executar + ls_tipo_produto;
-  }
-
-  // Envia o pacote para o V$PagueClient.
-  Send(ls_tags_executar);
-}
-
-function Conectar() {
-  // Retorna a conexão estabelecida.
-  return new WebSocket("ws://localhost:60906");
-}
-
-function Limpar() {
-  document.getElementById("output").innerHTML = "";
-}
-
-function SequencialAdicionar() {
-  in_sequencial++;
-  document.getElementById("io_txt_sequencial").value = in_sequencial;
-  return in_sequencial;
-}
-
-function Confirmar() {
-  var ls_tipo_transacao = document.getElementById("io_lst_transacao_tipo")
-    .options[io_lst_transacao_tipo.selectedIndex].text;
-
-  var ls_tags_executar =
-    'servico="executar"retorno="0"sequencial="' +
-    document.getElementById("io_txt_sequencial").value +
-    '"';
-
-  if (io_lst_transacao_tipo.selectedIndex >= 0) {
-    ls_tipo_transacao = 'transacao="' + ls_tipo_transacao + '"';
-    ls_tags_executar = ls_tags_executar + ls_tipo_transacao;
-  }
-
-  Send(ls_tags_executar);
-}
-
-function CartaoDigitar() {
-  Send(
-    'automacao_coleta_retorno="9"automacao_coleta_sequencial="' +
-      in_sequencial_executar +
-      '"'
-  );
-}
-
-function FluxoAbortar() {
-  Send(
-    'automacao_coleta_retorno="9"automacao_coleta_mensagem="Fluxo Abortado pelo operador!!"automacao_coleta_sequencial="' +
-      in_sequencial_executar +
-      '"'
-  );
-}
-
-function Mostrar(ao_event) {
-  var ls_tipo_servico = document.getElementById("io_lst_tipo_servico_mostrar")
-    .options[io_lst_tipo_servico_mostrar.selectedIndex].text;
-
-  if (ls_tipo_servico === "") {
-    ls_tipo_servico = document.getElementById("io_txt_servico_mostrar").text;
-  }
-
-  if (ao_event === "" || ao_event.keyCode === 13) {
-    Send(
-      'servico="mostrar"retorno="1"sequencial="' +
-        Sequencial() +
-        '"mensagem="' +
-        ls_tipo_servico +
+  abort() {
+    return this.send(
+      'automacao_coleta_retorno="9"automacao_coleta_mensagem="Fluxo Abortado pelo operador!!"automacao_coleta_sequencial="' +
+        this.in_sequencial_executar +
         '"'
     );
   }
-}
 
-function Coletar() {
-  var ls_tipo_servico = document.getElementById("io_lst_tipo_servico_coletar")
-    .options[io_lst_tipo_servico_coletar.selectedIndex].text;
+  confirm(transactionType) {
+    let ls_tipo_transacao = 'transacao="' + transactionType + '"';
 
-  Send(
-    'servico="coletar"retorno="1"sequencial="' +
-      Sequencial() +
-      '"mensagem="' +
-      ls_tipo_servico +
-      '"'
-  );
-}
+    let ls_tags_executar =
+      'servico="executar"retorno="0"sequencial="' +
+      this.in_sequencial +
+      '"' +
+      ls_tipo_transacao;
 
-function Perguntar() {
-  var ls_tipo_servico = document.getElementById("io_lst_tipo_servico_perguntar")
-    .options[io_lst_tipo_servico_perguntar.selectedIndex].text;
+    return this.send(ls_tags_executar);
+  }
 
-  Send(
-    'servico="perguntar"retorno="1"sequencial="' +
-      Sequencial() +
-      '"mensagem="' +
-      ls_tipo_servico +
-      "+" +
-      document.getElementById("io_txt_coleta_informacao").value +
-      '"'
-  );
-}
+  exec(opts) {
+    let {
+      ls_tipo_transacao,
+      ls_tipo_cartao,
+      ls_tipo_produto,
+      ls_transacao_pagamento,
+      ls_transacao_valor,
+    } = opts;
 
-function Consultar() {
-  Send('servico="consultar"retorno="0"sequencial="' + Sequencial() + '"');
+    let ls_tags_executar =
+      'servico="executar"retorno="1"sequencial="' + this.next() + '"';
+
+    if (ls_transacao_valor !== "") {
+      ls_transacao_valor = 'transacao_valor="' + ls_transacao_valor + '"';
+      ls_tags_executar = ls_tags_executar + ls_transacao_valor;
+    }
+
+    if (ls_tipo_transacao !== "") {
+      ls_tipo_transacao = 'transacao="' + ls_tipo_transacao + '"';
+      ls_tags_executar = ls_tags_executar + ls_tipo_transacao;
+    }
+
+    if (ls_tipo_cartao !== "") {
+      ls_tipo_cartao = 'transacao_tipo_cartao="' + ls_tipo_cartao + '"';
+      ls_tags_executar = ls_tags_executar + ls_tipo_cartao;
+    }
+
+    if (ls_tipo_produto !== "") {
+      ls_transacao_pagamento =
+        'transacao_pagamento="' + ls_transacao_pagamento + '"';
+      ls_tags_executar = ls_tags_executar + ls_transacao_pagamento;
+    }
+
+    if (ls_transacao_pagamento !== "") {
+      ls_tipo_produto = 'transacao_produto="' + ls_tipo_produto + '"';
+      ls_tags_executar = ls_tags_executar + ls_tipo_produto;
+    }
+
+    return this.send(ls_tags_executar);
+  }
+
+  connect({ onOpen, onMessage, onError, onClose, onSequencial }) {
+    this.io_connection = new WebSocket("ws://localhost:60906");
+
+    this.io_connection.onopen = function () {
+      this.io_tags = new Tags();
+      this.io_tags.init();
+
+      onOpen();
+    };
+
+    this.io_connection.onclose = function () {
+      onClose();
+    };
+
+    this.io_connection.onerror = function (error) {
+      console.error(error.message);
+      onError(error);
+    };
+
+    this.io_connection.onmessage = function (e) {
+      onMessage({ body: e.data });
+      this.io_tags.init();
+
+      // Mosta as tag's recebidas.
+      new ServicoDesmontar(this.io_tags, e.data);
+
+      // Se retorno não for de pacote ok...
+      if (this.io_tags.retorno !== "0") {
+        this.in_sequencial = this.io_tags.sequencial;
+        onSequencial(this.in_sequencial);
+      }
+
+      // Guarda o sequencial corrente da coleta.
+      this.in_sequencial = this.io_tags.sequencial;
+
+      // Apresenta o comprovante..
+      if (this.io_tags.transacao_comprovante_1via !== "") {
+        const comprovante =
+          this.io_tags.transacao_comprovante_1via +
+          this.io_tags.transacao_comprovante_2via;
+        alert(comprovante);
+      }
+    };
+  }
 }
 
 // Tags nessessárias para a integração.
-function Tags() {
-  var aplicacao;
-  var aplicacao_tela;
-  var estado;
-  var versao;
-  var mensagem;
-  var retorno;
-  var sequencial;
-  var servico;
-  var transacao;
-  var tipo_produto;
-  var transacao_comprovante_1via;
-  var transacao_comprovante_2via;
-  var transacao_comprovante_resumido;
-  var transacao_informacao;
-  var transacao_opcao;
-  var transacao_pagamento;
-  var transacao_parcela;
-  var transacao_produto;
-  var transacao_rede;
-  var transacao_tipo_cartao;
-  var transacao_administracao_usuario;
-  var transacao_administracao_senha;
-  var transacao_valor;
-  var automacao_coleta_sequencial;
-  var automacao_coleta_retorno;
-  var automacao_coleta_mensagem;
-  var automacao_coleta_informacao;
-  var automacao_coleta_opcao;
+class Tags {
+  aplicacao;
+  aplicacao_tela;
+  estado;
+  versao;
+  mensagem;
+  retorno;
+  sequencial;
+  servico;
+  transacao;
+  tipo_produto;
+  transacao_comprovante_1via;
+  transacao_comprovante_2via;
+  transacao_comprovante_resumido;
+  transacao_informacao;
+  transacao_opcao;
+  transacao_pagamento;
+  transacao_parcela;
+  transacao_produto;
+  transacao_rede;
+  transacao_tipo_cartao;
+  transacao_administracao_usuario;
+  transacao_administracao_senha;
+  transacao_valor;
+  automacao_coleta_opcao;
 
-  this.TagsAlimentar = function (as_tag, as_value) {
+  constructor() {
+    this.transacao_comprovante_1via = "";
+    this.transacao_comprovante_2via = "";
+    this.transacao = "";
+    this.transacao_produto = "";
+    this.servico = "";
+    this.retorno = 0;
+    this.sequencial = 0;
+  }
+
+  init() {
+    this.transacao_comprovante_1via = "";
+    this.transacao_comprovante_2via = "";
+    this.transacao = "";
+    this.transacao_produto = "";
+    this.servico = "";
+    this.retorno = 0;
+    this.sequencial = 0;
+  }
+
+  fill(as_tag, as_value) {
     if ("automacao_coleta_opcao" === as_tag) {
-      automacao_coleta_opcao = as_value;
+      this.automacao_coleta_opcao = as_value;
     } else if ("automacao_coleta_informacao" === as_tag) {
-      automacao_coleta_informacao = as_value;
+      this.automacao_coleta_informacao = as_value;
     } else if ("automacao_coleta_mensagem" === as_tag) {
-      automacao_coleta_mensagem = as_value;
+      this.automacao_coleta_mensagem = as_value;
     } else if ("automacao_coleta_retorno" === as_tag) {
-      automacao_coleta_retorno = as_value;
+      this.automacao_coleta_retorno = as_value;
     } else if ("automacao_coleta_sequencial" === as_tag) {
       this.automacao_coleta_sequencial = as_value;
     } else if ("transacao_comprovante_1via" === as_tag) {
@@ -302,22 +212,11 @@ function Tags() {
     } else if ("sequencial" === as_tag) {
       this.sequencial = parseInt(as_value, 0);
     }
-  };
-
-  this.TagsInicializar = function () {
-    this.transacao_comprovante_1via = "";
-    this.transacao_comprovante_2via = "";
-    this.transacao = "";
-    this.transacao_produto = "";
-    this.servico = "";
-    this.retorno = 0;
-    this.sequencial = 0;
-  };
+  }
 }
 
-var ServicoDesmontar = function (ao_servico) {
+var ServicoDesmontar = function (io_tags, ao_servico) {
   var ln_start = 0;
-
   var ln_end = ao_servico
     .toString()
     .indexOf("\n\r\n\t\t\r\n\t\t\t\r\n\t\t\r\n\t");
@@ -325,8 +224,6 @@ var ServicoDesmontar = function (ao_servico) {
   var ls_tag = "";
 
   var ls_value = "";
-
-  var ln_value_end = 0;
 
   try {
     // Enquanto não finalizou a leitura do pacote recebido...
@@ -351,7 +248,7 @@ var ServicoDesmontar = function (ao_servico) {
       ln_start += 2;
 
       // Alimenta com a tag recebida..
-      io_tags.TagsAlimentar(ls_tag, ls_value);
+      io_tags.fill(ls_tag, ls_value);
     }
 
     if (io_tags.servico !== "") {
@@ -389,18 +286,5 @@ var ServicoDesmontar = function (ao_servico) {
     }
   } catch (err) {
     alert("Error interno: " + err.message);
-  }
-
-  function JanelaFechar() {
-    Trace("Disconnect...");
-    io_connection.close();
-  }
-
-  function JanelaAbrir() {
-    Trace("Iniciando Aplicação Comercial...");
-
-    var loc = location.search.substring(1, location.search.length);
-
-    Trace(loc);
   }
 };
